4fa4c437507a28b7bce64673383db006442d6cf0
TheOgre <dogg123456789@gmail.com>
2025-08-25 10:33:27 -0400

    Adding import for Auth0Client

--- sharkiq/__init__.py
+++ sharkiq/__init__.py
@@ -1,6 +1,6 @@
 """Unofficial SDK for Shark IQ robot vacuums, designed primarily to support an integration for Home Assistant."""
 
-from .ayla_api import get_ayla_api, AylaApi
+from .ayla_api import AylaApi, get_ayla_api, Auth0Client
 from .exc import (
     SharkIqError,
     SharkIqAuthExpiringError,
